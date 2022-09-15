3be16409b14b0cf3f1b1ea0e1eadd33c70df7274
volodymyr35 <vzhukevych@gmail.com>
2022-09-15 23:13:24 +0300

    added home-page

--- src/modules/menu/components/menu-list/menu-list.component.tsx
+++ src/modules/menu/components/menu-list/menu-list.component.tsx
@@ -11,7 +11,7 @@ interface MenuListProps {
 
 export const MenuList: FC<MenuListProps> = ({ items }) => {
     return (
-        <div className="flex flex-wrap gap-10">
+        <div className="flex flex-wrap gap-10 justify-center">
             {items.map(({ image, ...pizza}) => (
                 <MenuItem {...pizza} 
                 imagePath={'/assets/pizza/' + image} 
